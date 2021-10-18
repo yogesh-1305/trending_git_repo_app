c3d8b2e21c3f16305ad477156a2ba10b4fe65608
Yogesh Singh <s.ashucool14@gmail.com>
2021-10-18 18:41:13 +0530

    add: repository api interface for making api requests

---
+++ app/src/main/java/com/example/trendinggitrepos/data/api/RepositoryApi.kt
@@ -0,0 +1,19 @@
+package com.example.trendinggitrepos.data.api
+
+import com.example.trendinggitrepos.data.model.Repository
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RepositoryApi {
+
+    @GET("/repositories")
+    suspend fun getRepositories(
+        @Query("language")
+        pLanguage: String = "java",
+        @Query("since")
+        since: String = "daily",
+        @Query("spoken_language_code")
+        languageCode: String
+    ): Response<Repository>
+}
